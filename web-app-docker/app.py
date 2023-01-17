ae872d70de42add628a6d14beeec2d1a5bbc8b50
Zeeshan <zeeshan.ijaz@emumba.com>
2023-01-17 21:01:43 +0500

    ip address stored in table inside db

--- web-app-docker/app.py
+++ web-app-docker/app.py
@@ -1,28 +1,34 @@
-import os
 from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
+from psycopg2 import connect
 
-app = Flask(_name_)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@host:port/dbname'
-db = SQLAlchemy(app)
-
-class Connection(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    ip_address = db.Column(db.String(255))
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    ip = request.remote_addr
-    connection = Connection(ip_address=ip)
-    db.session.add(connection)
-    db.session.commit()
-    return 'IP address saved: {}'.format(ip)
+    # Connect to the Postgres container
+    conn = connect(
+        host="172.17.0.2",
+        port=5432,
+        user="postgres",
+        password="postgres",
+        dbname="postgres"
+    )
 
-if _name_ == '_main_':
-    app.run(host="0.0.0.0", port=8080)
+    # Create the table to store IP addresses
+    with conn.cursor() as cur:
+        cur.execute("CREATE TABLE IF NOT EXISTS ip_addresses (id SERIAL PRIMARY KEY, address VARCHAR(15));")
 
+    # Store the incoming IP address
+    with conn.cursor() as cur:
+        cur.execute("INSERT INTO ip_addresses (address) VALUES (%s);", (request.remote_addr,))
 
+    conn.commit()
+    conn.close()
 
+    return "IP address stored!"
+
+if __name__ == '_main_':
+    app.run(debug=True, host='0.0.0.0',port=8080)
 
 
 
