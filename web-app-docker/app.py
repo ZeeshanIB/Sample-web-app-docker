bcfc7e591f5e9b03b7f526709a19114a799dc58b
Zeeshan <zeeshan.ijaz@emumba.com>
2023-01-20 00:30:15 +0500

    link db and webapp

--- web-app-docker/app.py
+++ web-app-docker/app.py
@@ -3,20 +3,16 @@ from psycopg2 import connect
 
 app = Flask(__name__)
 
-port=5432,
-host="hostname",
-user="username",
-password="password",
-dbname="database_name"
+
 @app.route('/')
 def index():
     # Connect to the Postgres container
     conn = connect(
-        port,
-        host,
-        user,
-        password,
-        dbname
+     port=5432,
+     host="hostname",
+     user="username",
+     password="password",
+     dbname="database_name"
     )
 
     # Create the table to store IP addresses
@@ -27,23 +23,14 @@ def index():
     with conn.cursor() as cur:
         cur.execute("INSERT INTO ip_addresses (address) VALUES (%s);", (request.remote_addr,))
 
-    conn.commit()
-    conn.close()
-    return "IP address stored!"
-@app.route('/print-table')
-def print_table():
-    conn = connect(
-        host="hostname",
-        user="username",
-        password="password",
-        dbname="database_name"
-    )
-    cur = conn.cursor()
+    
     cur.execute("SELECT * FROM ip_addresses")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    return "Table printed to console"
+    conn.commit()
+    conn.close()
+    return "IP address stored!"
 
 if __name__ == '_main_':
     app.run(debug=True, host='0.0.0.0',port=8080)
